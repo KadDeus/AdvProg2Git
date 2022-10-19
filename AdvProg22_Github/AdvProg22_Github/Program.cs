246000c5046e976d479b4ed1e6320588621c3d79
KadDeus <kadrinty@gmail.com>
2022-10-19 09:45:13 +0200

    Start Code (empty project)

---
+++ AdvProg22_Github/AdvProg22_Github/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvProg22_Github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
