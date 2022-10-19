85ffbc688f28b0598a965dac5272aba0be84690d
Yosh Cuypers <yosh.cuypers@student.kdg.be>
2022-10-19 10:46:59 +0200

    I made a different Print methode

--- AdvProg22_Github/AdvProg22_Github/Program.cs
+++ AdvProg22_Github/AdvProg22_Github/Program.cs
@@ -13,10 +13,27 @@ namespace AdvProg22_Github
             string yourName;
             Console.Write("Hello, what is your name: ");
             yourName = Console.ReadLine();
+
+            Print();
             Console.WriteLine("Your name is " + yourName);
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
+
+        public static void Print()
+        {
+            Random rnd = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 15);
+                for(int j = 10; j > 0; j--)
+                {
+                    Console.BackgroundColor = (ConsoleColor)rnd.Next(1, 15);
+                }
+            }
+        }
+
     }
 }
