05707c0341f8077f8cd726848cf5b02967ae461b
KadDeus <kadrinty@gmail.com>
2022-10-19 10:41:59 +0200

    Rainbow Print Method added

--- AdvProg22_Github/AdvProg22_Github/Program.cs
+++ AdvProg22_Github/AdvProg22_Github/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace AdvProg22_Github
 {
@@ -10,11 +11,37 @@ namespace AdvProg22_Github
     {
         static void Main(string[] args)
         {
+            PrintLine("test");
+            Print("Rest");
+
             Console.WriteLine("Hello World");
-            Console.WriteLine("Test");
 
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+        }
+
+        static void PrintLine(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Random r = new Random();
+                Console.ForegroundColor = (ConsoleColor)r.Next(1,14);
+                Console.Write(text.Substring(i, 1));
+                Thread.Sleep(20);
+                Console.ForegroundColor=ConsoleColor.White;
+            }
+            Console.WriteLine();
+        }
+        static void Print(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Random r = new Random();
+                Console.ForegroundColor = (ConsoleColor)r.Next(1, 14);
+                Console.Write(text.Substring(i, 1));
+                Thread.Sleep(20);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
 }
